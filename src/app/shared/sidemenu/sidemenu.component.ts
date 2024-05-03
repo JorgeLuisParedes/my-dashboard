f4eb9e2f5a54fe4cb14d9604da14e91fb46556c5
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-03 10:07:41 +0200

    Configuración del dashboard - Tailwind

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
-  selector: 'app-sidemenu',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidemenu.component.html',
-  styles: ``
+	selector: 'app-sidemenu',
+	standalone: true,
+	imports: [],
+	templateUrl: './sidemenu.component.html',
+	styles: ``,
 })
 export class SidemenuComponent {
+	public menuItems = routes
+		.map((route) => route.children ?? [])
+		.flat()
+		.filter((route) => route && route.path)
+		.filter((route) => !route.path?.includes(':'));
 
+	constructor() {
+		// const dashboardRoutes = routes
+		// 	.map((route) => route.children ?? [])
+		// 	.flat()
+		// 	.filter((route) => route && route.path)
+		// 	.filter((route) => !route.path?.includes(':'));
+		// console.log(dashboardRoutes);
+	}
 }
