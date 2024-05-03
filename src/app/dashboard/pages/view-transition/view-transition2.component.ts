a26db5b7cb89d1dfbd19fd8b310b104cfeb313ed
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-03 17:50:42 +0200

    ViewTransitions API

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+	standalone: true,
+	imports: [CommonModule, TitleComponent],
+	styles: ``,
+	template: `
+		<app-title title="View Transtion 2" />
+		<section class="flex justify-start">
+			<img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300" />
+			<div class="bg-blue-500 w-56 h-56"></div>
+		</section>
+	`,
+})
+export default class ViewTransitionComponent {}
