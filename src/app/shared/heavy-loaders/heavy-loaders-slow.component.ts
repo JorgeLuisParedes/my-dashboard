23e6d4694f7dbbc824f6356894a2d5b093fbfcb8
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-03 13:12:43 +0200

    Defer Blocks

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-heavy-loaders-slow',
 	standalone: true,
-	imports: [],
-	template: `<h1>Hola Mundo</h1>`,
+	imports: [CommonModule],
+	template: ` <section [ngClass]="['w-full h-[600px]', cssClass]">Heavy Loader Slow</section> `,
 	styles: ``,
 })
-export class HeavyLoadersSlowComponent {}
+export class HeavyLoadersSlowComponent {
+	@Input({ required: true }) cssClass!: string;
+
+	constructor() {
+		const start = Date.now();
+		while (Date.now() - start < 3000) {}
+		console.log('Cargado');
+	}
+}
