ed59aa90ea1691132f37c15c4d1259a3435c832c
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-03 18:59:23 +0200

    Servicios con señales

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '@services/users.service';
 
 @Component({
 	standalone: true,
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
 	templateUrl: './users.component.html',
 	styles: ``,
 })
-export default class UsersComponent {}
+export default class UsersComponent {
+	public userServices = inject(UsersService);
+}
