3f9986328b7a279b2bfe1404203a410fd35c6f6e
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-05-03 17:59:08 +0200

    Hero Animation - View Transition

--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -8,9 +8,9 @@ import { TitleComponent } from '@shared/title/title.component';
 	styles: ``,
 	template: `
 		<app-title title="View Transtion 2" />
-		<section class="flex justify-start">
-			<img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300" />
-			<div class="bg-blue-500 w-56 h-56"></div>
+		<section class="flex justify-end">
+			<img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300" style="view-transition-name: hero1" />
+			<div class="fixed bg-blue-800 w-32 h-32 bottom-16 right-10" style="view-transition-name: hero2"></div>
 		</section>
 	`,
 })
